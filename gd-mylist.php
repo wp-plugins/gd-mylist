36c2174f13b59110c42c86b97f566a856ced6ad4
andygi <andygi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-05 12:24:42 +0000

    fix wishlist's lista page of v0.2
    git-svn-id: https://plugins.svn.wordpress.org/gd-mylist/tags/0.2.1@1060364 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- gd-mylist.php
+++ gd-mylist.php
@@ -3,7 +3,7 @@
 Plugin Name: GD Mylist
 Plugin URI: https://wordpress.org/plugins/gd-mylist/
 Description: Create mylist items of posts and pages
-Version: 0.2
+Version: 0.2.1
 Author: Andy Greco
 Author URI: http://www.gekode.co.uk
 License: GPL
